61c93ed59f1c45991acc180451603b0015b12297
Uzma Haque Syeda <52292446+uzma0804@users.noreply.github.com>
2019-10-14 16:35:49 -0400

    Update line.js

--- line.js
+++ line.js
@@ -1,1 +1,34 @@
+//Load and parse data
+ 
+
+//Load data 
+ 
+
+
+
+//Inside get function:
+//Get the maximum and minimum Date and the maximum Price
+
+
+
+
+//define the width and height of the svg you will use 
+var width = //Insert value;
+var height = //Insert value;
+var margin= {
+  bottom://insert value
+ };
+
+
+//define SVG
+ 
+
+//Define the scales
+
+
+//Define the axes
+
+ 
+
+//Draw the line
 
